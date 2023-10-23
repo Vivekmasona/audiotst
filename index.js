e85c36edea9386b0d63b1fc0b3d4db90725e91bf
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-10-23 23:42:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,21 @@
 const express = require('express');
-const ytpl = require('ytpl');
 const app = express();
 const port = 3000;
 
-const playlistUrl = 'https://youtube.com/playlist?list=PLNhIv1Ws_Em7mYb98lZnY942a9y3Fnrz-&si=eBurDa4xpSQn99g4';
+// Define a route that accepts a "link" parameter
+app.get('/play', (req, res) => {
+  // Get the value of the "link" parameter from the request's query object
+  const link = req.query.link;
 
-let currentVideoIndex = 0;
+  if (!link) {
+    res.send('Please provide a valid link parameter.');
+  } else {
+    res.send(`You provided the link parameter: ${link}`);
+    // You can use the "link" value for further processing, such as playing audio or fetching content.
+  }
+});
 
-// Fetch the playlist information using ytpl
-ytpl(playlistUrl, { limit: 10 })
-  .then(playlistInfo => {
-    const videoUrls = playlistInfo.items.map(item => item.shortUrl);
-    const totalVideos = videoUrls.length;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
 
-    app.get('/', (req, res) => {
-      // Get the current video URL
-      const videoUrl = videoUrls[currentVideoIndex];
-
-      // Create an HTML page with an audio player
-      res.send(`
-        <html>
-          <body>
-            <h1>Playlist Audio Player</h1>
-            <p>Now playing: ${videoUrl}</p>
-            <audio controls autoplay>
-              <source src="${videoUrl}" type="audio/mpeg">
-            </audio>
-          </body>
-        </html>
-      `);
-
-      // Move to the next video when the current one ends
-      currentVideoIndex = (currentVideoIndex + 1) % totalVideos;
-    });
-
-    app.listen(port, () => {
-      console.log(`Server is listening on port ${port}`);
-    });
-  })
-  .catch(error => {
-    console.error('Error fetching playlist:', error);
-  });
