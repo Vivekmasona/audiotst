a0a53b55a0bcfc0c8537c6779cd321ff161ee6d3
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-10-23 23:38:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,48 +1,43 @@
 const express = require('express');
-const ytdl = require('ytdl-core');
+const ytpl = require('ytpl');
 const app = express();
 const port = 3000;
 
-const playlist = [
-  'youtube_video_url_1',
-  'youtube_video_url_2',
-  'youtube_video_url_3',
-  'youtube_video_url_4',
-  'youtube_video_url_5',
-  'youtube_video_url_6',
-  'youtube_video_url_7',
-  'youtube_video_url_8',
-  'youtube_video_url_9',
-  'youtube_video_url_10',
-  // Add more video URLs to your playlist
-];
+const playlistUrl = 'https://youtube.com/playlist?list=PLNhIv1Ws_Em7mYb98lZnY942a9y3Fnrz-&si=eBurDa4xpSQn99g4';
 
 let currentVideoIndex = 0;
 
-app.get('/', (req, res) => {
-  // Get the current video URL
-  const videoUrl = playlist[currentVideoIndex];
+// Fetch the playlist information using ytpl
+ytpl(playlistUrl, { limit: 10 })
+  .then(playlistInfo => {
+    const videoUrls = playlistInfo.items.map(item => item.shortUrl);
+    const totalVideos = videoUrls.length;
 
-  // Get the audio stream URL using ytdl-core
-  const stream = ytdl(videoUrl, { filter: 'audioonly' });
+    app.get('/', (req, res) => {
+      // Get the current video URL
+      const videoUrl = videoUrls[currentVideoIndex];
 
-  // Create an HTML page with an audio player
-  res.send(`
-    <html>
-      <body>
-        <h1>Playlist Audio Player</h1>
-        <p>Now playing: ${videoUrl}</p>
-        <audio controls autoplay>
-          <source src="${stream}" type="audio/mp4">
-        </audio>
-      </body>
-    </html>
-  `);
+      // Create an HTML page with an audio player
+      res.send(`
+        <html>
+          <body>
+            <h1>Playlist Audio Player</h1>
+            <p>Now playing: ${videoUrl}</p>
+            <audio controls autoplay>
+              <source src="${videoUrl}" type="audio/mpeg">
+            </audio>
+          </body>
+        </html>
+      `);
 
-  // Move to the next video when the current one ends
-  currentVideoIndex = (currentVideoIndex + 1) % playlist.length;
-});
+      // Move to the next video when the current one ends
+      currentVideoIndex = (currentVideoIndex + 1) % totalVideos;
+    });
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}`);
+    });
+  })
+  .catch(error => {
+    console.error('Error fetching playlist:', error);
+  });
