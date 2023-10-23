c9998872a4ed819f60d6f22a38ea07098ad3caa2
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-10-23 23:46:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 const express = require('express');
+const ytpl = require('ytpl');
 const app = express();
 const port = 3000;
+let videoUrls = [];
+let currentVideoIndex = 0;
 
-// Define a route that accepts a "link" parameter
-app.get('/play', (req, res) => {
-  // Get the value of the "link" parameter from the request's query object
-  const link = req.query.link;
+app.get('/', async (req, res) => {
+  try {
+    if (videoUrls.length === 0) {
+      const playlistId = 'PLNhIv1Ws_Em7mYb98lZnY942a9y3Fnrz-'; // Replace with your playlist ID
+      const playlistInfo = await ytpl(playlistId, { limit: Infinity });
+      videoUrls = playlistInfo.items.map(item => item.shortUrl);
+    }
 
-  if (!link) {
-    res.send('Please provide a valid link parameter.');
-  } else {
-    res.send(`You provided the link parameter: ${link}`);
-    // You can use the "link" value for further processing, such as playing audio or fetching content.
+    if (currentVideoIndex < videoUrls.length) {
+      const videoUrl = videoUrls[currentVideoIndex];
+      currentVideoIndex++;
+
+      // Create an HTML page with an audio player
+      res.send(`
+        <html>
+          <body>
+            <h1>Playlist Audio Player</h1>
+            <p>Now playing: ${videoUrl}</p>
+            <audio controls autoplay>
+              <source src="${videoUrl}" type="audio/mpeg">
+            </audio>
+          </body>
+        </html>
+      `);
+    } else {
+      res.send('Playlist completed.');
+    }
+  } catch (error) {
+    res.status(500).send('Error: ' + error.message);
   }
 });
 
