d9fe4fcdf1caf75522b1d69521a556039f283f29
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-10-24 17:18:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,29 @@
-from pytube import YouTube
-from flask import Flask, session, url_for, send_file, render_template, redirect, request
-from io import BytesIO
+const express = require('express');
+const axios = require('axios');
+const fs = require('fs');
+const app = express();
+const port = 3000;
 
-app = Flask(__name__)
-app.config["SECRET_KEY"] = "my_secret_key"
+app.get('/img', async (req, res) => {
+  const ytLink = req.query.url; // Get the URL parameter 'url'
 
-@app.route("/", methods=["POST", "GET"])
-def index():
-    if request.method == "POST":
-        session["link"] = request.form.get("url")
-        url = YouTube(session["link"])
-        url.check_availability()
-        return render_template("download.html", url=url)
+  try {
+    // Fetch the image from the provided URL (e.g., a YouTube thumbnail)
+    const response = await axios.get(ytLink, { responseType: 'stream' });
 
-    return render_template('index.html')
-@app.route('/download', methods=['GET'])
-def download_playlist():
-    playlist_url = request.args.get('url')  # Get the 'url' parameter from the URL
-    if not playlist_url:
-        return "Please provide a valid playlist URL."
+    // Set the response headers
+    res.setHeader('Content-Type', response.headers['content-type']);
+    res.setHeader('Content-Disposition', 'attachment; filename=image.jpg'); // You can adjust the filename if needed
 
-    try:
-        playlist = Playlist(playlist_url)
-        # Download all the videos from the playlist
-        for video_url in playlist.video_urls:
-            yt = YouTube(video_url)
-            audio_stream = yt.streams.filter(only_audio=True).first()
-            audio_stream.download(output_path='/path/to/save/audio')
+    // Pipe the image to the response
+    response.data.pipe(res);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('Error');
+  }
+});
 
-        return "Playlist downloaded successfully!"
-    except Exception as e:
-        return f"An error occurred: {str(e)}"
-
-if __name__ == '__main__':
-    app.run()
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
