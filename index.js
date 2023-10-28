c9bcb62ca9e3a5c4ec97cb7650aae0c27a9650ab
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-10-28 22:16:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,20 @@ const ytdl = require('ytdl-core');
 const app = express();
 const port = 3000;
 
-app.get('/stream', (req, res) => {
-  const videoURL = req.query.url; // YouTube video ka URL
+app.get('/play', (req, res) => {
+  const videoURL = 'LIVE_VIDEO_URL_HERE'; // Live video ka URL yahan par replace kare
+
+  // YouTube video ko audio me fetch kare
+  const audioStream = ytdl(videoURL, { filter: 'audioonly' });
+
+  // Set the appropriate content type for audio
   res.header('Content-Type', 'audio/mpeg');
 
-  ytdl(videoURL, { filter: 'audioonly' }).pipe(res);
+  // Pipe audio stream to response
+  audioStream.pipe(res);
 });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
