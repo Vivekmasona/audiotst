95707ea5ad0c16808eddc51d48640cc57ec7bca8
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-10-29 23:08:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,45 @@
 const express = require('express');
 const ytdl = require('ytdl-core');
 const fs = require('fs');
-const axios = require('axios');
+const https = require('https'); // Import the 'https' module
 
 const app = express();
 const port = 3000;
 
+const commonPosterUrl = 'https://i.ytimg.com/vi/5oExKMYIE9U/maxresdefault.jpg'; // Common poster URL for all songs
+
 app.get('/download', async (req, res) => {
   try {
     const videoUrl = req.query.videoUrl;
-    const posterUrl = req.query.posterUrl;
 
-    // Fetch the poster image
-    const response = await axios.get(posterUrl, { responseType: 'arraybuffer' });
-    const posterData = response.data;
+    // Fetch the common poster image using 'https' module
+    https.get(commonPosterUrl, (posterResponse) => {
+      const posterData = [];
+      posterResponse.on('data', (chunk) => {
+        posterData.push(chunk);
+      });
+      posterResponse.on('end', () => {
+        const posterBuffer = Buffer.concat(posterData);
 
-    const info = await ytdl.getInfo(videoUrl);
-    const audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
-    
-    if (audioFormats.length === 0) {
-      return res.send('No audio formats found for this video.');
-    }
-    
-    const audioFormat = audioFormats[0];
-    const audioStream = ytdl(videoUrl, { quality: audioFormat.itag });
-    const filename = `${info.videoDetails.title}.mp3`;
+        const info = ytdl.getInfo(videoUrl);
+        const audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
+        
+        if (audioFormats.length === 0) {
+          return res.send('No audio formats found for this video.');
+        }
+        
+        const audioFormat = audioFormats[0];
+        const audioStream = ytdl(videoUrl, { quality: audioFormat.itag });
+        const filename = `${info.videoDetails.title}.mp3`;
 
-    audioStream.pipe(fs.createWriteStream(filename));
-    
-    // Combine the audio file and poster here
-    // You can use 'posterData' and 'filename' to customize the combination
-    
-    return res.send('Audio downloaded successfully with poster.');
+        audioStream.pipe(fs.createWriteStream(filename));
+        
+        // Combine the audio file and common poster here
+        // You can use 'posterBuffer' and 'filename' to customize the combination
+        
+        return res.send('Audio downloaded successfully with common poster.');
+      });
+    });
   } catch (error) {
     return res.send('Error: ' + error);
   }
