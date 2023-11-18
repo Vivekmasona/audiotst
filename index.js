81d9a421c792bed1455d5135fc496d0b9a95b0a7
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-11-18 20:37:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,20 @@
-const express = require('express');
-const ytdl = require('ytdl-core');
-const fs = require('fs');
+const ytdl = require("ytdl-core");
 
-const app = express();
-const port = 3000;
+export default async function handler(req, res) {
+  const { url } = req.query;
 
-const commonPosterUrl = 'https://i.ytimg.com/vi/5oExKMYIE9U/maxresdefault.jpg'; // Common poster URL for all songs
+  if (!url) return res.status(400).json({ error: "No YouTube link provided" });
 
-app.get('/download', async (req, res) => {
   try {
-    const videoUrl = req.query.videoUrl;
-
-    const info = await ytdl.getInfo(videoUrl);
-    const audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
-    
-    if (audioFormats.length === 0) {
-      return res.send('No audio formats found for this video.');
-    }
-    
-    const audioFormat = audioFormats[0];
-    const audioStream = ytdl(videoUrl, { quality: audioFormat.itag });
-    const filename = `${info.videoDetails.title}.mp3`;
-
-    audioStream.pipe(fs.createWriteStream(filename));
-    
-    // Combine the audio file with the common poster here
-    // You can use 'commonPosterUrl' and 'filename' to customize the combination
-    
-    return res.send('Audio downloaded successfully with common poster.');
-  } catch (error) {
-    return res.send('Error: ' + error);
+    ytdl(url, {
+      filter: function (format) {
+        return !format.hasVideo;
+      },
+      quality: "highest",
+    }).pipe(res);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ error: "Something went wrong" });
   }
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+}
 
