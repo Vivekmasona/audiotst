addd9dfcea08947063c1c73497ea6079f8755891
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-12-09 19:02:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
-const ytdl = require("ytdl-core");
+const express = require('express');
+const fetch = require('node-fetch');
+const app = express();
 
-export default async function handler(req, res) {
-  const { url } = req.query;
+app.get('/download', async (req, res) => {
+  const ytUrl = req.query.url;
+  if (!ytUrl) {
+    return res.status(400).send('Error: YouTube URL is missing in the query parameter.');
+  }
 
-  if (!url) return res.status(400).json({ error: "No YouTube link provided" });
+  const apiUrl = `https://youtube-mp36.p.rapidapi.com/dl?id=${encodeURIComponent(ytUrl)}`;
+  const rapidApiKey = '11939ea42cmsh9be181f6708fc39p162794jsn9e46f87d898b';
+
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': rapidApiKey,
+      'X-RapidAPI-Host': 'youtube-mp36.p.rapidapi.com',
+    },
+  };
 
   try {
-    ytdl(url, {
-      filter: function (format) {
-        return !format.hasVideo;
-      },
-      quality: "highest",
-    }).pipe(res);
-  } catch (e) {
-    console.log(e);
-    res.status(500).json({ error: "Something went wrong" });
+    const response = await fetch(apiUrl, options);
+    const result = await response.text();
+    res.send(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
   }
-}
+});
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
